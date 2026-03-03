b644f0d76a42037fd7ec2d5a2ffa4395876ea5c6
Cristian F. Taborda <tabordacristianfernando@gmail.com>
2026-03-02 21:59:58 -0300

    main

--- api/main.py
+++ api/main.py
@@ -61,7 +61,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.CORS_ORIGINS,        
+    allow_origins=[
+        "https://ethernal.fund",
+        "http://localhost:5173",  # para desarrollo local
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
