a02f4047b5301adee52560a750a9f7aad95e6a04
Cristian F. Taborda <tabordacristianfernando@gmail.com>
2026-04-08 16:15:24 -0300

    alembic

--- api/db/models/__init__.py
+++ api/db/models/__init__.py
@@ -1,2 +1,8 @@
+# api/db/models/__init__.py
+from api.db.models.user import User
+from api.db.models.fund import PersonalFund
+from api.db.models.transaction import Transaction
+from api.db.models.treasury import FeeRecord, EarlyRetirementRequest
+from api.db.models.protocol import DeFiProtocol
 from api.db.models.contact import ContactMessage
 from api.db.models.survey import AnonymousSurvey, SurveyFollowUp
